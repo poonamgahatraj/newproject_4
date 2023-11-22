d479ce2e86e7a51bb3986dfa508d48eb8bbc7386
Poonam Gahatraj <poonamgtrz@gmail.com>
2023-11-22 11:47:05 +0545

    newproject_4

---
+++ newproject_4/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import helowrld from './components/helowrld.vue'
+
+
+</script>
+
+<template>
+
+ <helowrld/>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
